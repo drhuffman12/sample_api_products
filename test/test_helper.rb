88e77b5e7a355eb58ed17c451b0ae5da951510c3
Daniel Huffman <drhuffman12@yahoo.com>
2015-05-29 18:52:09 -0400

    prep for Mongoid

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,13 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+
+  setup {
+    Mongoid.default_session.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+  }
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
