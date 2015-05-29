8323c79c1b5e78a668ed43e25a4d16a6d47dfcf1
Daniel Huffman <drhuffman12@yahoo.com>
2015-05-29 18:50:26 -0400

    Create a script to populate the DB with the products.json file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'json'
+
+def load_products
+  file_path = 'db/products.json'
+  log_path = file_path + '.log'
+  error_path = file_path + '.err'
+  errored_qty = 0
+  loaded_qty = 0
+  begin
+    File.delete(log_path) if File.exists?(log_path)
+    File.delete(error_path) if File.exists?(error_path)
+    file = File.read(file_path)
+    json_data = JSON.parse(file)
+    json_data['products'].each do |jd|
+      begin
+        created = Product.create!(jd)
+        loaded_qty += 1
+        File.open(log_path, 'a') {|f| f << "#{jd.inspect}\n" }
+      rescue => e
+        errored_qty += 1
+        File.open(error_path, 'a') {|f| f << "#{{data: jd, error_message: e.message, error_backtrace: e.backtrace}.inspect}\n" }
+      end
+    end
+  rescue => e
+    errored_qty += 1
+    File.open(error_path, 'a') {|f| f << "#{{error_message: e.message, error_backtrace: e.backtrace}.inspect}\n" }
+  end
+  File.open(log_path, 'a') { |f| f.write("\n\n#{loaded_qty} loaded") } if loaded_qty > 0
+  File.open(error_path, 'a') { |f| f.write("\n\n#{errored_qty} errored") } if errored_qty > 0
+end
+
+load_products
