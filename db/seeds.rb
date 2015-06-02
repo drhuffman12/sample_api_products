34f7f8cbe28acbdcf567a42681ad262133701219
Daniel Huffman <drhuffman12@yahoo.com>
2015-06-02 15:39:35 -0400

    * added a force-failure record * made dataloader logging prettier

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,24 +17,48 @@ def load_products
   begin
     File.delete(log_path) if File.exists?(log_path)
     File.delete(error_path) if File.exists?(error_path)
+    when_started = DateTime.now
+    when_started_str = "Started: #{when_started}"
+    File.open(log_path, 'a') {|f| f << when_started_str }
+    File.open(error_path, 'a') {|f| f << when_started_str }
+
     file = File.read(file_path)
     json_data = JSON.parse(file)
+
+    record_count = json_data['products'].to_a.length
+
+    File.open(log_path, 'a') {|f| f << "\n\nLoading..." }
     json_data['products'].each do |jd|
       begin
         created = Product.create!(jd)
         loaded_qty += 1
-        File.open(log_path, 'a') {|f| f << "#{jd.inspect}\n" }
+        File.open(log_path, 'a') {|f| f << "\n\n#{jd.inspect}" }
       rescue => e
         errored_qty += 1
-        File.open(error_path, 'a') {|f| f << "#{{data: jd, error_message: e.message, error_backtrace: e.backtrace}.inspect}\n" }
+        error_msg = "\n\n" + '='*80 + "\n== Error:#{e.message}"
+        error_msg << "\n\n== Data:\n#{jd}"
+        error_msg << "\n\n== Error Backtrace:\n#{e.backtrace}"
+        File.open(error_path, 'a') {|f| f << error_msg }
       end
     end
   rescue => e
     errored_qty += 1
     File.open(error_path, 'a') {|f| f << "#{{error_message: e.message, error_backtrace: e.backtrace}.inspect}\n" }
   end
-  File.open(log_path, 'a') { |f| f.write("\n\n#{loaded_qty} loaded") } if loaded_qty > 0
-  File.open(error_path, 'a') { |f| f.write("\n\n#{errored_qty} errored") } if errored_qty > 0
+  File.open(log_path, 'a') { |f| f.write("\n\n#{loaded_qty} out of #{record_count} loaded.") }
+  File.open(error_path, 'a') { |f| f.write("\n\n#{errored_qty} out of #{record_count} errored.") }
+
+  File.open(log_path, 'a') { |f| f.write("\n\nWarning! Not all data loaded! See '#{error_path}'.") } if (loaded_qty < record_count)
+
+  when_finished = DateTime.now
+  when_finished_str = "\n\nFinished: #{when_finished}"
+  when_duration = ((when_finished - when_started) * 24.0 * 60 * 60).to_f
+  when_duration_str = "\nDuration: #{format("%.4f", when_duration)} seconds\n"
+
+  File.open(log_path, 'a') {|f| f << when_finished_str }
+  File.open(error_path, 'a') {|f| f << when_finished_str }
+  File.open(log_path, 'a') {|f| f << when_duration_str }
+  File.open(error_path, 'a') {|f| f << when_duration_str }
 end
 
 load_products
