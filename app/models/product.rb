fa366ee48232824ab61a3cb1e8f7af56c93160f9
Daniel Huffman <drhuffman12@yahoo.com>
2015-06-02 15:40:13 -0400

    * added timestamping * removed extra comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,10 @@ class Product
   field :height, type: Integer
   field :weight, type: Integer
 
+  include Mongoid::Timestamps
+
   validates_uniqueness_of :name, :scope => [:type]
-  validates_presence_of :name, :type # , :length, :width, :height, :weight
+  validates_presence_of :name, :type
   validates_numericality_of :length, :greater_than => 0
   validates_numericality_of :width, :greater_than => 0
   validates_numericality_of :height, :greater_than => 0
